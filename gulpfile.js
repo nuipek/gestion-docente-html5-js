f374ed9115d184b12ff5ee1ce25c1273432c8402
Urko Villanueva <uvillanueva@ipartek.com>
2017-05-11 08:14:36 +0200

    Referencia de proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@
  */
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var babili = require("gulp-babili");
 
 gulp.task("babel", function () {
     return gulp.src("src/app.js")
         .pipe(babel())
+        .pipe(babili({
+            mangle: {
+                keepClassNames: true
+            }
+        }))
         .pipe(gulp.dest("dist"));
 });
