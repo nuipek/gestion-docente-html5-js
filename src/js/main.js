de78401d0207e230159cb39bd3334b49676f3b10
Urko Villanueva <uvillanueva@ipartek.com>
2017-05-11 14:45:01 +0200

    Referencia de proyecto

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var alumnos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+var alumnos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
 $.noConflict();
 jQuery(document).ready(function($) {
     // Code that uses jQuery's $ can follow here.
